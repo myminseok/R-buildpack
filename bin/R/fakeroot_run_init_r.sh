130bed03ad4f4fb4891d091fc2a9362d12439ef9
myminseok <myminseok@gmail.com>
2017-08-25 18:06:01 +0900

    adding init.r runner

---
+++ bin/R/fakeroot_run_init_r.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. /root/.bashrc
+
+# fix R for staging env
+cd /usr/lib/R/etc
+rm Renviron
+cp Renviron.orig Renviron
+rm Makeconf
+ln -s /etc/R/Makeconf Makeconf
+rm ldpaths
+ln -s /etc/R/ldpaths ldpaths
+
+export EDITOR=vim
+R --no-save --quiet --slave --file=$1
+
+# R -e "install.packages('shiny', repos='http://cran.us.r-project.org')"
+exit
--- test/shiny/run.sh
+++ test/shiny/run.sh
@@ -2,4 +2,7 @@
 source /root/.bashrc
 
 source /.profile.d/r_environment.sh
+## 
+mkdir -p ./src/contrib/PACKAGES
+
 R -e "shiny::runApp('/001-hello', port=8080, host='0.0.0.0')"
