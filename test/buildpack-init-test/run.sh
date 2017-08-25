e253a3d7d59917b689a7764040919e41d9b870a5
myminseok <myminseok@gmail.com>
2017-08-25 17:24:58 +0900

    update sample

---
+++ test/buildpack-init-test/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. /root/.bashrc
+
+. /.profile.d/r_environment.sh
+Rscript /prog.r
--- test/shiny/run.sh
+++ test/shiny/run.sh
@@ -2,4 +2,4 @@
 source /root/.bashrc
 
 source /.profile.d/r_environment.sh
-R -e "shiny::runApp('/shiny/001-hello', port=8080, host='0.0.0.0')"
+R -e "shiny::runApp('/001-hello', port=8080, host='0.0.0.0')"
